cd26b81c07d14fb7494b08c9b1c89a9793b84897
Edgar <edgar.muoni@gmail.com>
2026-01-19 11:58:12 +0200

    Fetching SchoolList data from SQL ReactCRUD

--- reactcrud.client/src/views/SchoolList.tsx
+++ reactcrud.client/src/views/SchoolList.tsx
@@ -11,48 +11,43 @@ interface School {
 function SchoolList() {
     const [schools, setSchools] = useState<School[]>([]);
 
-    const fetchSchools = useCallback(async () => {
-        const response = await fetch("/api/schools");
-        const data = await response.json();
-        setSchools(data);
-    }, []);
-
     useEffect(() => {
         populateSchoolData();
-    }, [fetchSchools]);
-
-    <table>
-        <div>
-            <h1>School List</h1>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Address</th>
-                    <th>Student Count</th>
-                </tr>
-            </thead>
-            <tbody>
-                {schools.map((school) => (
-                    <tr key={school.id}>
-                        <td>{school.id}</td>
-                        <td>{school.name}</td>
-                        <td>{school.address}</td>
-                        <td>{school.studentCount}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </div>
-    </table>;
-
+    }, []);
 
     async function populateSchoolData() {
-        const response = await fetch("schoolsListViewModel");
+        const response = await fetch("/api/school");
         if (response.ok) {
             const data = await response.json();
             setSchools(data);
         }
     }
+
+    return (
+        <div>
+            <h1>School List</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Student Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {schools.map((school) => (
+                        <tr key={school.id}>
+                            <td>{school.id}</td>
+                            <td>{school.name}</td>
+                            <td>{school.address}</td>
+                            <td>{school.studentCount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default SchoolList;
